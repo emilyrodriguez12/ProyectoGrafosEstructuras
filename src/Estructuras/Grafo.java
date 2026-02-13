1c17c245a5a908c6b5f45bad133b56d64dbc6fb1
SaracualD <daniel.saracual@correo.unimet.edu.ve>
2026-02-13 12:30:33 -0400

    grafo

---
+++ src/Estructuras/Grafo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Grafo {
+    Lista_Proteinas vertices;
+    
+    
+    public Grafo(){
+        this.vertices = new Lista_Proteinas();
+    }
+}
